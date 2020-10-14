20607705e296dd6d7eed621f183dcf31dbcc248e
StanMandela <smstan8@gmail.com>
2020-10-14 22:12:40 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -37,17 +37,15 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-//    public void createMapIntent(View view){
-//        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
-//        mapIntent.setPackage("com.google.android.apps.maps");
-//        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(mapIntent);
-//        }
-//    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri uri= Uri.parse("tel:0123456789");
+        Intent phoneIntent= new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 
 }
